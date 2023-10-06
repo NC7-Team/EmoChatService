9bee647c359ac980e360788eaff5f2c805e51c42
data04190 <data04190@naver.com>
2023-10-07 00:19:02 +0900

    init

---
+++ src/main/java/com/example/emotion_chat/EmotionChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.emotion_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionChatApplication.class, args);
+	}
+
+}
