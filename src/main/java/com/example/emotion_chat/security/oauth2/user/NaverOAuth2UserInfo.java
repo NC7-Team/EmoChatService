e702d5a26414892f3c4f1e67e9a55a6380057ad7
same-woo <pardongwoo@naver.com>
2023-10-23 23:50:03 +0900

    "fix : social login"

--- src/main/java/com/example/emotion_chat/security/oauth2/user/NaverOAuth2UserInfo.java
+++ src/main/java/com/example/emotion_chat/security/oauth2/user/NaverOAuth2UserInfo.java
@@ -9,7 +9,7 @@ public class NaverOAuth2UserInfo extends OAuth2UserInfo {
     public NaverOAuth2UserInfo(Map<String, Object> attributes) { super(attributes); }
 
     @Override
-    public String getId() {return attributes.get("id").toString();}
+    public String getId() {return (String) attributes.get("id");}
 
     @Override
     public String getEmail() {return (String) attributes.get("email");}
