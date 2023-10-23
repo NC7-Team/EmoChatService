abac5927b37b11e684e9635f66c95b1a30a72a21
same-woo <pardongwoo@naver.com>
2023-10-23 20:41:04 +0900

    "fix : social login"

--- src/main/java/com/example/emotion_chat/security/oauth2/user/NaverOAuth2UserInfo.java
+++ src/main/java/com/example/emotion_chat/security/oauth2/user/NaverOAuth2UserInfo.java
@@ -3,24 +3,33 @@ package com.example.emotion_chat.security.oauth2.user;
 import java.util.Map;
 
 public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+    private Map<String, Object> attributes;
 
     public NaverOAuth2UserInfo(Map<String, Object> attributes) {
         super(attributes);
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
     public String getId() {
-        return (String) attributes.get("sub");
-    }
-
-    @Override
-    public String getName() {
-        return (String) attributes.get("name");
+        // Access the ID directly from the attributes map
+        return attributes.get("id").toString();
     }
 
     @Override
     public String getEmail() {
-        return (String) attributes.get("email");
+        // Access the email directly from the attributes map
+        return attributes.get("email").toString();
     }
 
+    @Override
+    public String getName() {
+        // Access the name directly from the attributes map
+        return attributes.get("name").toString();
+    }
 }
