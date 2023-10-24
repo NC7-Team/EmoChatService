ea15053ad8478ce4f1c92dcdc93f58481d61caa3
오동현 <dhgus44@naver.com>
2023-10-24 21:30:14 +0900

    절대 건들지 말것 유일한채팅

--- src/main/java/com/example/emotion_chat/controller/DiaryController.java
+++ src/main/java/com/example/emotion_chat/controller/DiaryController.java
@@ -1,40 +1,40 @@
-package com.example.emotion_chat.controller;
-
-import com.example.emotion_chat.entity.Diary;
-import com.example.emotion_chat.service.DiaryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/diary")
-public class DiaryController {
-  @Autowired
-  private DiaryService diaryService;
-
-  @PostMapping
-  public Diary createDiary(@RequestBody Diary diary) {
-    return diaryService.createDiary(diary);
-  }
-
-  @GetMapping("/user/{userId}")
-  public List<Diary> getUserDiaries(@PathVariable Long userId) {
-    return diaryService.getUserDiaries(userId);
-  }
-
-  @GetMapping("/{id}")
-  public Diary getDiaryById(@PathVariable Long id) {
-    return diaryService.getDiaryById(id);
-  }
-
-  @PutMapping("/{id}")
-  public Diary updateDiary(@PathVariable Long id, @RequestBody Diary updatedDiary) {
-    return diaryService.updateDiary(id, updatedDiary);
-  }
-
-  @DeleteMapping("/{id}")
-  public void deleteDiary(@PathVariable Long id) {
-    diaryService.deleteDiary(id);
-  }
-}
+//package com.example.emotion_chat.controller;
+//
+//import com.example.emotion_chat.entity.Diary;
+//import com.example.emotion_chat.service.DiaryService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/diary")
+//public class DiaryController {
+//  @Autowired
+//  private DiaryService diaryService;
+//
+//  @PostMapping
+//  public Diary createDiary(@RequestBody Diary diary) {
+//    return diaryService.createDiary(diary);
+//  }
+//
+//  @GetMapping("/user/{userId}")
+//  public List<Diary> getUserDiaries(@PathVariable Long userId) {
+//    return diaryService.getUserDiaries(userId);
+//  }
+//
+//  @GetMapping("/{id}")
+//  public Diary getDiaryById(@PathVariable Long id) {
+//    return diaryService.getDiaryById(id);
+//  }
+//
+//  @PutMapping("/{id}")
+//  public Diary updateDiary(@PathVariable Long id, @RequestBody Diary updatedDiary) {
+//    return diaryService.updateDiary(id, updatedDiary);
+//  }
+//
+//  @DeleteMapping("/{id}")
+//  public void deleteDiary(@PathVariable Long id) {
+//    diaryService.deleteDiary(id);
+//  }
+//}
--- src/main/java/com/example/emotion_chat/service/DiaryService.java
+++ src/main/java/com/example/emotion_chat/service/DiaryService.java
@@ -1,48 +1,48 @@
-package com.example.emotion_chat.service;
-
-import com.example.emotion_chat.entity.Diary;
-import com.example.emotion_chat.entity.User;
-import com.example.emotion_chat.repository.DiaryRepository;
-import com.example.emotion_chat.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.*;
-
-@Service
-public class DiaryService {
-  @Autowired
-  private DiaryRepository diaryRepository;
-  @Autowired
-  private UserRepository userRepository;
-
-  public Diary createDiary(Diary diary) {
-    return diaryRepository.save(diary);
-  }
-
-  public List<Diary> getUserDiaries(Long userId) {
-    User user = userRepository.findById(userId).orElse(null);
-    if (user != null) {
-      return diaryRepository.findByUser(user);
-    }
-    return Collections.emptyList();
-  }
-
-  public Diary getDiaryById(Long id) {
-    return diaryRepository.findById(id).orElse(null);
-  }
-
-  public Diary updateDiary(Long id, Diary updatedDiary) {
-    Diary diary = diaryRepository.findById(id).orElse(null);
-    if (diary != null) {
-      diary.setEmotion(updatedDiary.getEmotion());
-      diary.setContent(updatedDiary.getContent());
-      return diaryRepository.save(diary);
-    }
-    return null;
-  }
-
-  public void deleteDiary(Long id) {
-    diaryRepository.deleteById(id);
-  }
-}
+//package com.example.emotion_chat.service;
+//
+//import com.example.emotion_chat.entity.Diary;
+//import com.example.emotion_chat.entity.User;
+//import com.example.emotion_chat.repository.DiaryRepository;
+//import com.example.emotion_chat.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.*;
+//
+//@Service
+//public class DiaryService {
+//  @Autowired
+//  private DiaryRepository diaryRepository;
+//  @Autowired
+//  private UserRepository userRepository;
+//
+//  public Diary createDiary(Diary diary) {
+//    return diaryRepository.save(diary);
+//  }
+//
+//  public List<Diary> getUserDiaries(Long userId) {
+//    User user = userRepository.findById(userId).orElse(null);
+//    if (user != null) {
+//      return diaryRepository.findByUser(user);
+//    }
+//    return Collections.emptyList();
+//  }
+//
+//  public Diary getDiaryById(Long id) {
+//    return diaryRepository.findById(id).orElse(null);
+//  }
+//
+//  public Diary updateDiary(Long id, Diary updatedDiary) {
+//    Diary diary = diaryRepository.findById(id).orElse(null);
+//    if (diary != null) {
+//      diary.setEmotion(updatedDiary.getEmotion());
+//      diary.setContent(updatedDiary.getContent());
+//      return diaryRepository.save(diary);
+//    }
+//    return null;
+//  }
+//
+//  public void deleteDiary(Long id) {
+//    diaryRepository.deleteById(id);
+//  }
+//}
