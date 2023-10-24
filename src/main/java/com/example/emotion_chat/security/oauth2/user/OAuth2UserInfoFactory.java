0d44253666dd2205f1bba30c2b7108fcc0df7f6f
same-woo <pardongwoo@naver.com>
2023-10-24 14:26:49 +0900

    "feat : 깃허브 로그인 추가"

--- src/main/java/com/example/emotion_chat/security/oauth2/user/OAuth2UserInfoFactory.java
+++ src/main/java/com/example/emotion_chat/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -10,8 +10,8 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         if(registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
             return new GoogleOAuth2UserInfo(attributes);
-        } else if (registrationId.equalsIgnoreCase(AuthProvider.facebook.toString())) {
-            return new FacebookOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
+            return new GithubOAuth2UserInfo(attributes);
         } else if (registrationId.equalsIgnoreCase(AuthProvider.kakao.toString())) {
             return new KakaoOAuth2UserInfo(attributes);
         } else if (registrationId.equalsIgnoreCase(AuthProvider.naver.toString())) {
